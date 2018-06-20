9524bc9cc015dcb39c7fe524a3022990ae99f942
jiejie152 <jwang152@163.com>
2018-06-20 16:56:10 +0800

    first commit

---
+++ src/main/java/com/wj/spring/Service1.java
@@ -0,0 +1,24 @@
+package com.wj.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Service1 {
+
+	@ResponseBody
+	@RequestMapping(value="/")
+	public String home() {   
+        return "Hello World!";
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Service1.class,args);
+	}
+
+}
